de976ba6a264a4ef1a49a066063f78a9f70faa8a
RUAN JOSE DUARTE DA SILVA <ruan.jdsilva@senacsp.edu.br>
2025-05-12 12:53:31 -0300

    aumentando logo

--- app/src/main/java/com/example/myapplication/components/Logo.kt
+++ app/src/main/java/com/example/myapplication/components/Logo.kt
@@ -1,9 +1,12 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.R
 
 @Composable
@@ -12,6 +15,8 @@ fun Logo(modifier: Modifier = Modifier) {
         painter = painterResource(id = R.drawable.nitro_logo_bgt),
         contentDescription = "Nitro Logo",
         modifier = Modifier
+        .width(80.dp)
+        .height(60.dp)
     )
 }
 
